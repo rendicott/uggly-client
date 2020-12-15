a178b793cfb70d53b73e0f006679239f69f2492a
Russell Endicott <rendicott@gmail.com>
2020-12-14 22:15:00 -0500

    initial commit, compiles

---
+++ go.mod
@@ -0,0 +1,12 @@
+module uggly-client
+
+go 1.15
+
+replace github.com/rendicott/uggly => ../uggly
+
+require (
+	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/rendicott/uggly v0.0.1
+	google.golang.org/grpc v1.34.0 // indirect
+	google.golang.org/grpc/examples v0.0.0-20201214170045-17e2cbe88713 // indirect
+)
---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/examples/data"
+	pb "github.com/rendicott/uggly"
+)
+
+var (
+	tls                = flag.Bool("tls", false, "Connection uses TLS if true, else plain TCP")
+	caFile             = flag.String("ca_file", "", "The file containing the CA root cert file")
+	serverAddr         = flag.String("server_addr", "localhost:10000", "The server address in the format of host:port")
+	serverHostOverride = flag.String("server_host_override", "x.test.youtube.com", "The server name used to verify the hostname returned by the TLS handshake")
+)
+
+func main() {
+	flag.Parse()
+	var opts []grpc.DialOption
+	if *tls {
+		if *caFile == "" {
+			*caFile = data.Path("x509/ca_cert.pem")
+		}
+		creds, err := credentials.NewClientTLSFromFile(*caFile, *serverHostOverride)
+		if err != nil {
+			log.Fatalf("Failed to create TLS credentials %v", err)
+		}
+		opts = append(opts, grpc.WithTransportCredentials(creds))
+	} else {
+		opts = append(opts, grpc.WithInsecure())
+	}
+	opts = append(opts, grpc.WithBlock())
+	conn, err := grpc.Dial(*serverAddr, opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	defer conn.Close()
+	_ = pb.NewScreenerClient(conn)
+}
